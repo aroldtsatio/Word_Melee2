191615d3abe50e008af352dde1577a39551e7ca5
Arold <aroldtsatio@gmail.com>
2025-03-25 18:01:51 +0100

    first Screen the App Choose Language   Create Room and Join Room

--- Word_Melee2/src/App.jsx
+++ Word_Melee2/src/App.jsx
@@ -1,16 +1,32 @@
 
-import './App.css'
+import './App.css';
+import React from 'react';
+import ChooseLanguage from './components/ChooseLanguage';
+import CreateRooms from './components/CreateRooms';
+import RoomList from './components/RoomList';
 
 function App() {
-  
-
+  console.log("App.jsx");
+  const [language, setLanguage] = React.useState("en");
+  const [rooms, setRooms] = React.useState([
+    { id: "ABCDEF", name: "Room 1" },
+    { id: "GHIJKL", name: "Room 2" },
+    { id: "MNOPQR", name: "Room 3" },
+  ]);
   return (
-    <>
-       <h1 class="text-3xl font-bold underline bg-red-400 p-2"> 
-    Hello world!
-  </h1>
-    </>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+       {/* Choose Language */}
+       <ChooseLanguage language={language} setLanguage={setLanguage} />
+
+
+        {/* Create Rooms */}
+        <CreateRooms setRooms={setRooms}/>
+
+        {/* Room List */}
+        <RoomList rooms={rooms} />
+    </div> 
+    
+  );
 }
 
-export default App
+export default App;
